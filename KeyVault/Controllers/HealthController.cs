29839559ce1e0d8ed3d196187db41a28d4004cd7
Chris Gårdenberg <webmaster@itssimple.se>
2020-06-03 13:09:30 +0200

    feat: Added HealthController (dummy data right now)

---
+++ KeyVault/Controllers/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KeyVault.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        public object IsHealthy()
+        {
+            return new
+            {
+                DataSource = true,
+                BackendServices = true
+            };
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 I'm reviving the old idea that a coworker and me had back in 2012.
 
 I will fill this readme out a bit later, when I have gotten somewhere with the project.
+
+---
+
+### Supporters of this project / initiative
+
+- My spare time / vactions
+- Coffee
+- Energy drinks
