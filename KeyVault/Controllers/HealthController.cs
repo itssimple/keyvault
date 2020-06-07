4037dbff05e3017f0107a4333732a52482a7f5f2
Chris Gårdenberg <webmaster@itssimple.se>
2020-06-07 22:09:07 +0200

    fix: Just output the name (subject)

--- KeyVault/Controllers/HealthController.cs
+++ KeyVault/Controllers/HealthController.cs
@@ -26,7 +26,7 @@ namespace KeyVault.Controllers
         [Route("whoami")]
         public object WhoAmI()
         {
-            return User.Identity;
+            return User.Identity.Name;
         }
     }
 }
